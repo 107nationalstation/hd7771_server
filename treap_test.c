fb841daa32ce657eca23ff93e55b6d0e3f6fe1c3
107nationalstation <yyclxx123@163.com>
2016-09-15 16:14:22 +0800

    update

--- treap_test.c
+++ treap_test.c
@@ -9,23 +9,19 @@ int main(void)
 	treap_t *root = NULL;
 
 	for (int i = 0; i < 10240; i++) {
-		//printf("Inserting %d \n", i);
 		root = insert(root, i);
 	}
 
 	printf("InOrder traversal of input treap\n");
-	//printInOrder(root);
 	printf("\n");
 
-	printf("Delete 10, 12\n");
+	printf("Delete first, first\n");
 	root = delete(root, first(root));
-	//root = delete(root, 0);
 	printf("First now %d\n", first(root));
 	root = delete(root, first(root));
 	printf("First now %d\n", first(root));
 
 	printf("InOrder traversal of input treap post deletion\n");
-	//printInOrder(root);
 
 	return 0;
 
